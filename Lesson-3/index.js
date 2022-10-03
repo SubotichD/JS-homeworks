bea43481df97c4317c9544d367a0817a62f7a42d
Subotch Daria <dacha_subotich.ru>
2022-10-03 15:06:08 +0300

    new files

--- Lesson-3/index.js
+++ Lesson-3/index.js
@@ -64,3 +64,79 @@ for (let i = 8; i <= 56; i++) {
 
 
 
+                    // Массивы
+                        // 1 exers
+                    
+ let arr = [1,2,3,4,5];
+ for (let i = 0; i < arr.length; i++){
+    console.log(arr[i]);
+    // document.write(arr[i]);
+ }
+
+
+                // 2 exers
+
+
+let ar = [-2, -1, -3, 15, 0, -4, 2, -5, 9, -15, 0, 4, 5, -6, 10, 7] ; 
+
+for(i = 0; i<arr.length; i++){
+ if(ar[i] > 10 && arr[i] < -3){
+    console.log(ar[i]+'<br>');
+ }
+    
+    }
+
+                    // 3 exers
+
+
+
+                    // 4 exers
+                   
+let erray = ['10', '20', '30', '50', '235', '3000']; 
+console.log(erray[0] + erray[1] + erray[3]);                  
+
+
+                            // 5 exers
+
+let week = [ 'Mn', 'Tu', 'We', 'Th', 'Fr', 'St', 'Sn']   
+for (i = 0; i < week.length; i++) {
+   if(i > 3) {
+    document.write(week[i] + '<b>' + ' ');
+   } else {
+    document.write(week[i] + ' ');
+   }
+}  
+
+
+                    // 6 exers  ?????????
+
+let stroyk = [12, 'Name', 9, 12, 'Daria'];
+stroyk[stroyk.length - 1];
+console.log(stroyk);  
+
+                // 8 exers
+
+let arrrr = [12, false, 'Текст', 4, 2, -5, 0];
+ 
+for (let i = arrrr.length - 1; i >= 0; i--) {
+    console.log(arrrr[i]);
+}
+
+
+
+
+                                    // Функции 
+
+            // 1 exers   
+                                 
+
+
+
+
+
+
+
+ 
+
+
+
